d0353a1cb49f119856c090591cddbd489f31aab6
Obi-Wan3 <44986166+Obi-Wan3@users.noreply.github.com>
2021-01-18 11:03:48 -0800

    [Translate] use alpha version of googletrans to fix NoneType error

--- translate/translate.py
+++ translate/translate.py
@@ -20,7 +20,7 @@ class Translate(commands.Cog):
             language = "zh-cn"
         try:
             res = translator.translate(message, dest=language)
-        except ValueError:
+        except (ValueError, AttributeError):
             failed_embed = discord.Embed(description="Translation failed.", color=discord.Color.red())
             return await ctx.channel.send(embed=failed_embed)
         translated_embed.add_field(name=googletrans.LANGUAGES[res.src.lower()].title(), value=res.origin, inline=True)
