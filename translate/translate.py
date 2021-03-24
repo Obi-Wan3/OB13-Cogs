79d0080fef120b9057ae9cf68cd920196b16298e
Obi-Wan3 <44986166+Obi-Wan3@users.noreply.github.com>
2021-03-23 19:02:49 -0700

    [Translate] improvements

--- translate/translate.py
+++ translate/translate.py
@@ -27,6 +27,8 @@ import googletrans
 import discord
 from redbot.core import commands
 
+TRANSLATOR = googletrans.Translator()
+
 
 class Translate(commands.Cog):
     """
@@ -42,17 +44,20 @@ class Translate(commands.Cog):
     @commands.command(name="translate")
     async def _translate(self, ctx: commands.Context, language, *, message):
         """Translate some text."""
-        translator = googletrans.Translator()
-        translated_embed = discord.Embed(title='Translation', color=discord.Color.green())
+
         if language.lower() in googletrans.LANGUAGES:
             language = googletrans.LANGUAGES[language.lower()]
-        elif language.lower() in ("zh", "ch") or language.lower() == "chinese":
+        elif language.lower() in ("zh", "ch", "chinese"):
             language = "zh-cn"
+
         try:
-            res = translator.translate(message, dest=language)
+            res = TRANSLATOR.translate(message, dest=language)
         except (ValueError, AttributeError):
             failed_embed = discord.Embed(description="Translation failed.", color=discord.Color.red())
             return await ctx.channel.send(embed=failed_embed)
+
+        translated_embed = discord.Embed(title='Translation', color=discord.Color.green())
         translated_embed.add_field(name=googletrans.LANGUAGES[res.src.lower()].title(), value=res.origin, inline=True)
         translated_embed.add_field(name=googletrans.LANGUAGES[res.dest.lower()].title(), value=res.text, inline=True)
+
         return await ctx.channel.send(embed=translated_embed)
