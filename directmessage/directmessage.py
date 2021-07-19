2439981d752516afe61837422ded286736c7d2ed
Onii-chan <69662328+Onii-Chan-Discord@users.noreply.github.com>
2021-07-19 14:17:31 +1000

    Update directmessage.py

--- directmessage/directmessage.py
+++ directmessage/directmessage.py
@@ -24,16 +24,30 @@ SOFTWARE.
 
 import discord
 from redbot.core import commands
+import logging
+
+log = logging.getLogger("red.obi.directmessage")
 
 
 class DirectMessage(commands.Cog):
     """Send DMs as the bot!"""
+   
+    def cog_unload(self):
+        global dm
+        if invite:
+            try:
+                self.bot.remove_command("dm")
+            except Exception as e:
+                log.info(e)
+            self.bot.add_command(dm)
+        if self.startup_task:
+            self.startup_task.cancel() 
 
     def __init__(self, bot):
         self.bot = bot
 
     @commands.is_owner()
-    @commands.command(name="directmessage", aliases=["sdm"])
+    @commands.command(name="dm", aliases=["sdm", "directmessage"])
     async def _direct_message(self, ctx: commands.Context, user: discord.User, *, message):
         """Sends a DM to a user (sends raw text directly)."""
         try:
