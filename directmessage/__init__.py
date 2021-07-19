4369711589224355558234c383cac4a7c07d018e
Onii-chan <69662328+Onii-Chan-Discord@users.noreply.github.com>
2021-07-19 14:20:45 +1000

    Update __init__.py

--- directmessage/__init__.py
+++ directmessage/__init__.py
@@ -1,10 +1,6 @@
 import json
 from pathlib import Path
-from .directmessage import DirectMessage
+from .directmessage import setup
 
 with open(Path(__file__).parent / "info.json") as fp:
     __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
-
-
-async def setup(bot):
-    bot.add_cog(DirectMessage(bot))
