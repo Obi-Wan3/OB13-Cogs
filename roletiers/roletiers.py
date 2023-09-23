385a54a2c20513b8a1da142b9882260fdaa2bdda
Jademalo <386846+Jademalo@users.noreply.github.com>
2023-09-23 09:05:10 +0100

    [RoleTiers] Fix imports

--- roletiers/roletiers.py
+++ roletiers/roletiers.py
@@ -22,7 +22,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
-from datetime import datetime, timedelta
+from datetime import datetime, timezone, timedelta
 
 import discord
 from discord.ext import tasks
